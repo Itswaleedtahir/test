ea16a42f744e3186346e336c602ea57de2caa74b
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-09-29 15:17:09 +0500

    Create index.js

---
+++ collection/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+app.use(express.json());
+const routes = require('./routes/index')
+
+app.use('/api/user',routes);
+
+
+mongoose.connect('mongodb+srv://itswaleedtahir:itswaleedtahir123@cluster0.smf6v.mongodb.net/interview').then(result=>{
+    console.log("database connected")
+}).catch(err=>{
+    console.log(err)
+})
+
+
+app.listen(8000);
